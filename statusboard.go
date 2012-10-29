b7112d7ba989b560fb8737d441ec8c68fe908a17
root <root@dev-centos.(none)>
2012-10-28 22:07:06 -0500

    Added options handler

---
+++ statusboard.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
+)
+
+const jsonFile = "statuses.json"
+const debug = true
+
+func printHelp() {
+	args := os.Args
+	fmt.Printf("USAGE:\n")
+	fmt.Printf("statusboard: ACTION OPTIONS\n")
+	fmt.Printf("\t update ITEM STATUS\n")
+}
+
+func createEmptyDb() {
+	m := make(map[string]string)
+	b, json_err := json.Marshal(m)
+	if json_err != nil { panic(json_err) }
+	write_err := ioutil.WriteFile(jsonFile, b, 0644)
+	if write_err != nil { panic(write_err) }
+}
+
+func updateStatus(item string, status string) {
+	// check if db exists - if not create a blank one
+	_, err := os.Stat(jsonFile)
+	if err != nil { createEmptyDb() }	
+
+	// create any empty map to hold json contents
+	m := make(map[string]string)
+
+	// read current statues file
+	r, read_err := ioutil.ReadFile(jsonFile)
+	if read_err != nil { panic(read_err) }	
+
+	// unmarshall data read in from file
+	unmarshall_error:= json.Unmarshal(r, &m)
+	if unmarshall_error != nil { panic(unmarshall_error) }
+
+	// set map with the passed in data
+	m[item] = status
+
+	// marshall data
+	b, marshall_error := json.Marshal(m)
+	if marshall_error != nil { panic(marshall_error) }
+
+	// write json data tile file
+	write_error := ioutil.WriteFile(jsonFile, b, 0644)
+	if write_error !=nil { panic(write_error) }	
+}
+
+func main () {
+	args := os.Args
+	if len(args) > 1  {
+		action := args[1]
+		switch action {
+		case "update":
+			if len(args) == 4 {
+				item, status := args[2], args[3]
+				updateStatus(item, status)
+			} else {
+				printHelp()
+			}
+		default:
+			printHelp()
+		}
+
+	} else {
+		printHelp()
+	}
+}
