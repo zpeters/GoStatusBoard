4006f6df50af0190a6555bbb55520a24395d96d5
root <root@dev-centos.(none)>
2012-10-28 21:46:45 -0500

    Added checking/creation of a blank file if needed.  Writing statuses works now

--- status.go
+++ status.go
@@ -7,18 +7,37 @@ import (
 )
 
 const jsonFile = "statuses.json"
+const debug = true
 
 func updateStatus(item string, status string) {
-	r, err := ioutil.ReadFile(jsonFile)
-	if err != nil { panic(err) }	
-	var m map[string]string
-	err2 := json.Unmarshal(r, &m)
-	if err2 != nil { panic(err2) }
+	_, err := os.Stat(jsonFile)
+	if err != nil {
+		// file doesn't exist, create an empty json file
+		// create an empty map
+			m := make(map[string]string)
+		// marshall to a empty json object
+		b, json_err := json.Marshal(m)
+		if json_err != nil { panic(json_err) }
+		// write out to file
+		write_err := ioutil.WriteFile(jsonFile, b, 0644)
+		if write_err != nil { panic(write_err) }
+	}	
+	// read current statues file
+	r, read_err := ioutil.ReadFile(jsonFile)
+	if read_err != nil { panic(read_err) }	
+	// create any empty map to hold json contents
+	m := make(map[string]string)
+	// unmarshall data read in from file
+	unmarshall_error:= json.Unmarshal(r, &m)
+	if unmarshall_error != nil { panic(unmarshall_error) }
+	// set map with the passed in data
 	m[item] = status
-	b, err3 := json.Marshal(m)
-	if err3 != nil { panic(err3) }
-	err4 := ioutil.WriteFile(jsonFile, b, 0644)
-	if err4 !=nil { panic(err4) }
+	// marshall data
+	b, marshall_error := json.Marshal(m)
+	if marshall_error != nil { panic(marshall_error) }
+	// write json data tile file
+	write_error := ioutil.WriteFile(jsonFile, b, 0644)
+	if write_error !=nil { panic(write_error) }
 	
 }
 
@@ -26,5 +45,7 @@ func main () {
 	args := os.Args
 	if len(args) == 3 {
 		updateStatus(args[1], args[2])
+	} else {
+		panic("Specify Item and Status")
 	}
 }
